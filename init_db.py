0affec42b914c0fbe546c19b2d4325f27c66fb3e
Mohamed <mohamed@MacBook-Air-de-Mohamed.local>
2024-10-30 21:45:03 +0100

    Mise à jour de la table 'exercises' avec les colonnes question et answer.

--- init_db.py
+++ init_db.py
@@ -1,3 +1,4 @@
+# pylint:disable=(astroid-error)
 """
 Ce module initialise et gère la base de données DuckDB pour l'application.
 
@@ -14,10 +15,21 @@ def create_exercise_table_in_db(con: duckdb.DuckDBPyConnection) -> None:
 
     :param con: Connexion active à la base de données DuckDB.
     """
-    # Exemples de données pour les thèmes et exercices
+    # Exemples de données pour les thèmes, exercices, questions et réponses
     data = {
         "theme": ["SQL Basics", "Joins", "Aggregation"],
         "exercise_name": ["Exercice 1", "Exercice 2", "Exercice 3"],
+        "question": [
+            "Sélectionnez tous les employés.",
+            "Affichez les employés avec leur département.",
+            "Calculez le salaire moyen par département.",
+        ],
+        "answer": [
+            "SELECT * FROM employees;",
+            "SELECT employees.name, department.department_name FROM employees JOIN "
+            "department ON employees.department = department.id;",
+            "SELECT department, AVG(salary) FROM employees GROUP BY department;",
+        ],
     }
     exercises_df = pd.DataFrame(data)  # pylint: disable=unused-variable
 
