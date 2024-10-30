326b166f436d2d2cdc967655ec953a303e1ba6cb
Mohamed <mohamed@MacBook-Air-de-Mohamed.local>
2024-10-30 19:20:07 +0100

    Ajout de la fonction 'init_database' pour initialiser la base de données et créer le dossier de données si nécessaire.

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ charge les exercices et les solutions SQL, et fournit une interface
 interactive avec Streamlit pour que les utilisateurs puissent pratiquer SQL.
 """
 
+import os
+
 import duckdb
 
 
@@ -16,3 +18,19 @@ def connect_db(name: str) -> duckdb.DuckDBPyConnection:
     :returns: Objet de connexion à la base de données.
     """
     return duckdb.connect(database=f"./data/{name}", read_only=False)
+
+
+def init_database(name: str) -> None:
+    """Initialise la base de données en créant le dossier de données et les tables nécessaires.
+
+    :param name: Nom du fichier de base de données.
+    """
+    # Crée le dossier s'il n'existe pas
+    if "data" not in os.listdir():
+        os.mkdir("data")
+
+    # Initialise la base de données si le fichier n'existe pas
+    if name not in os.listdir("data"):
+        con = connect_db(name)
+        # Ajout d'un appel fictif pour la création de tables (à remplacer plus tard)
+        con.close()
